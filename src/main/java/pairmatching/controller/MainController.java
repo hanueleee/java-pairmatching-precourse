d2546651f25e6a2a6aaebb1a168108c197ac27c6
hanueleee <ezzeezz@naver.com>
2022-12-16 15:34:44 +0900

    feat(controller): 페어매칭관리 애플리케이션의 메인로직 구현

--- src/main/java/pairmatching/controller/MainController.java
+++ src/main/java/pairmatching/controller/MainController.java
@@ -1,4 +1,35 @@
 package pairmatching.controller;
 
+import pairmatching.domain.PairFinder;
+import pairmatching.domain.PairInitializer;
+import pairmatching.domain.PairMatcher;
+import pairmatching.repository.PairRecordRepository;
+import pairmatching.view.InputView;
+import pairmatching.view.OutputView;
+
 public class MainController {
+    private final InputView inputView = new InputView();
+    private final OutputView outputView = new OutputView();
+    private final PairMatcher pairMatcher = new PairMatcher();
+    private final PairFinder pairFinder = new PairFinder();
+    private final PairInitializer pairInitializer = new PairInitializer();
+
+    public void programRun() {
+        String option = "";
+
+        while (option != "Q") {
+            outputView.printOption();
+            option = inputView.readOption();
+            if (option == "1") {
+                pairMatcher.run();
+            }
+            if (option == "2") {
+                pairFinder.run();
+            }
+            if (option == "3") {
+                pairInitializer.run();
+            }
+        }
+
+    }
 }
